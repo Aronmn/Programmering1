7b13e12325f7b335f9c922979f7f24ee68f2a62e
Aron <aronmn@vgy.se>
2016-08-30 15:04:47 +0200

    Övn1-4

--- Variabler/Program.cs
+++ Variabler/Program.cs
@@ -10,6 +10,32 @@ namespace Variabler
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Mata in ett heltal");
+            string inmatat;
+            inmatat = Console.ReadLine();
+            int mittHeltal = int.Parse(inmatat);
+            Console.WriteLine("Mata in ett heltal:");
+            inmatat = Console.ReadLine();
+            double mittDecimaltal = double.Parse(inmatat);
+            Console.WriteLine("summan av talen blir: " + (mittHeltal + mittDecimaltal));
+            Console.WriteLine("produkten av talen blir: " + (mittHeltal * mittDecimaltal));
+            Console.WriteLine("kvoten av talen blir: " + (mittHeltal / mittDecimaltal));
+            Console.ReadLine();
+            /*ovning 2 */
+            Console.WriteLine("Mata in ditt förnamn:");
+            string fnamn;
+            fnamn = Console.ReadLine();
+            Console.WriteLine("Mata in ditt efternamn:");
+            string enamn;
+            enamn = Console.ReadLine();
+            Console.WriteLine("Hej " + (fnamn + enamn) + ("!"));
+            /*ovning 3*/
+            Console.WriteLine("Mata in grader i celsius:");
+            string grader;
+            grader = Console.ReadLine();
+            int degcel = int.Parse(grader);
+            Console.WriteLine("Det blir " + (degcel + 1.8 * +32) + (" farenheit"));
+            /*ovning 4*/
         }
     }
 }
